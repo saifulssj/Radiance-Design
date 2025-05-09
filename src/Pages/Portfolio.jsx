83be19908a1ec7ccd6503c0c4f7fcc929937b836
saifulssj <105748536+saifulssj@users.noreply.github.com>
2025-05-10 02:36:10 +0600

    Update Portfolio.jsx
    just a spelling check

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import portfolioData from "../data/portfolioData";
+import portfolioData from "../data/portfoliodata";
 
 const Portfolio = () => {
   return (
