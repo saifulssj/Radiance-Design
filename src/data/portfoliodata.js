16f4e4915ee0d4b4806ca958698a4ae44733dd5e
Md. Saifullah sheikh moon <105748536+saifulssj@users.noreply.github.com>
2025-07-10 18:28:09 +0600

    Update portfoliodata.js

--- src/data/portfoliodata.js
+++ src/data/portfoliodata.js
@@ -65,7 +65,7 @@ export const flyerDesignData = [
     
   },  {
     title: "Creative Flyer 8",
-    image: "/public/assets/flyer design/Creative Flyer 8.jpg",
+    image: "/assets/flyer design/Creative Flyer 8.jpg",
     
   },  {
     title: "Creative Flyer 9",
@@ -85,52 +85,52 @@ export const labelAndPackagingData = [
     images: [
   {
     title: "label and packaging 1",
-    image: "/public/assets/label and packaging/6c4ce6176965293.Y3JvcCwxNzA0LDEzMzMsMTQ3LDA.jpg",
+    image: "/assets/label and packaging/6c4ce6176965293.Y3JvcCwxNzA0LDEzMzMsMTQ3LDA.jpg",
     
   },
     {
     title: "label and packaging 2",
-    image: "/public/assets/label and packaging/8d87f9187857673.Y3JvcCwxOTY4LDE1NDAsMTE2LDA (1).jpg",
+    image: "/assets/label and packaging/8d87f9187857673.Y3JvcCwxOTY4LDE1NDAsMTE2LDA (1).jpg",
     
   },
     {
     title: "label and packaging 3",
-    image: "/public/assets/label and packaging/24d8c0216159905.677bc61431975.png",
+    image: "/assets/label and packaging/24d8c0216159905.677bc61431975.png",
     
   },
     {
     title: "label and packaging 4",
-    image: "/public/assets/label and packaging/82cda7175824761.Y3JvcCwxOTE3LDE1MDAsNDIsMA.jpg",
+    image: "/assets/label and packaging/82cda7175824761.Y3JvcCwxOTE3LDE1MDAsNDIsMA.jpg",
     
   },
     {
     title: "label and packaging 5",
-    image: "/public/assets/label and packaging/387ff3176965755.Y3JvcCwzOTk5LDMxMjgsMCwzOQ.png",
+    image: "/assets/label and packaging/387ff3176965755.Y3JvcCwzOTk5LDMxMjgsMCwzOQ.png",
     
   },
     {
     title: "label and packaging 6",
-    image: "/public/assets/label and packaging/9015bb200379069.66615e75a4cb7.jpg",
+    image: "/assets/label and packaging/9015bb200379069.66615e75a4cb7.jpg",
     
   },
     {
     title: "label and packaging 7",
-    image: "/public/assets/label and packaging/bbe65f175824395.Y3JvcCwyNTAwLDE5NTUsMCwxMjM.jpg",
+    image: "/assets/label and packaging/bbe65f175824395.Y3JvcCwyNTAwLDE5NTUsMCwxMjM.jpg",
     
   },
     {
     title: "label and packaging 8",
-    image: "/public/assets/label and packaging/c30a8b200379069.66615e75a5aa8.jpg",
+    image: "/assets/label and packaging/c30a8b200379069.66615e75a5aa8.jpg",
     
   },
     {
     title: "label and packaging 9",
-    image: "/public/assets/label and packaging/e9a233176966773.Y3JvcCwzODM1LDMwMDAsMzMyLDA (1).png",
+    image: "/assets/label and packaging/e9a233176966773.Y3JvcCwzODM1LDMwMDAsMzMyLDA (1).png",
     
   },
     {
     title: "label and packaging 10",
-    image: "/public/assets/label and packaging/f6ccaf200379069.66615e75a52ce.png",
+    image: "/assets/label and packaging/f6ccaf200379069.66615e75a52ce.png",
     
   },
 
@@ -142,27 +142,27 @@ export const BrandIdentityData = [
     images: [
   {
     title: "Brand Identity 1",
-    image: "/public/assets/logo and branding/5d006e208547083.Y3JvcCwxNjE2LDEyNjQsMCww (1).png",
+    image: "/assets/logo and branding/5d006e208547083.Y3JvcCwxNjE2LDEyNjQsMCww (1).png",
     
   },
     {
     title: "Brand Identity 2",
-    image: "/public/assets/logo and branding/76effb217568897.Y3JvcCwyMDIwLDE1ODAsMCww (1).png",
+    image: "/assets/logo and branding/76effb217568897.Y3JvcCwyMDIwLDE1ODAsMCww (1).png",
     
   },
     {
     title: "Brand Identity 3",
-    image: "/public/assets/logo and branding/cd4731215610591.Y3JvcCwyMDIwLDE1ODAsMCww (1).jpg",
+    image: "/assets/logo and branding/cd4731215610591.Y3JvcCwyMDIwLDE1ODAsMCww (1).jpg",
     
   },
     {
     title: "Brand Identity 4",
-    image: "/public/assets/logo and branding/cd8e26202364327.Y3JvcCwyMDIwLDE1ODAsMCww (1).jpg",
+    image: "/assets/logo and branding/cd8e26202364327.Y3JvcCwyMDIwLDE1ODAsMCww (1).jpg",
     
   },
     {
     title: "Brand Identity 5",
-    image: "/public/assets/logo and branding/f88e5d215558085.Y3JvcCwyMDIwLDE1ODAsMCww (1).jpg",
+    image: "/assets/logo and branding/f88e5d215558085.Y3JvcCwyMDIwLDE1ODAsMCww (1).jpg",
     
   },
     
@@ -175,47 +175,47 @@ export const menuDesignData = [
     images: [
   {
     title: "Menu Design 1",
-    image: "/public/assets/menu/22ca29202991411.668f70597814d (1).jpg",
+    image: "/assets/menu/22ca29202991411.668f70597814d (1).jpg",
     
   },
     {
     title: "Menu Design 2",
-    image: "/public/assets/menu/5a9146199833403.6658303f83b9c.jpg",
+    image: "/assets/menu/5a9146199833403.6658303f83b9c.jpg",
     
   },
     {
     title: "Menu Design 3",
-    image: "/public/assets/menu/5c3639202991411.668f705976e43 (1).jpg",
+    image: "/assets/menu/5c3639202991411.668f705976e43 (1).jpg",
     
   },
     {
     title: "Menu Design 4",
-    image: "/public/assets/menu/8b7dff202991411.668f7059792de (1).jpg",
+    image: "/assets/menu/8b7dff202991411.668f7059792de (1).jpg",
     
   },
     {
     title: "Menu Design 5",
-    image: "/public/assets/menu/a6ed98199833403.6658303f83381.jpg",
+    image: "/assets/menu/a6ed98199833403.6658303f83381.jpg",
     
   },
     {
     title: "Menu Design 6",
-    image: "/public/assets/menu/a7b965202991411.668f705977750 (1).jpg",
+    image: "/assets/menu/a7b965202991411.668f705977750 (1).jpg",
 
   },
     {
     title: "Menu Design 7",
-    image: "/public/assets/menu/bacae3199833403.6658303f823c5.jpg",
+    image: "/assets/menu/bacae3199833403.6658303f823c5.jpg",
 
   },
     {
     title: "Menu Design 8",
-    image: "/public/assets/menu/ce8df5199833403.6658303f82dda.jpg",
+    image: "/assets/menu/ce8df5199833403.6658303f82dda.jpg",
     
   },
     {
     title: "Menu Design 9",
-    image: "/public/assets/menu/da29db202991411.668f705979c55 (1).jpg",
+    image: "/assets/menu/da29db202991411.668f705979c55 (1).jpg",
     
   },
 
@@ -227,52 +227,52 @@ export const signageDesignData = [
     images: [
   {
     title: "signage Design 1",
-    image: "/public/assets/signage design/Behance-cover Digital Marketing.jpg",
+    image: "/assets/signage design/Behance-cover Digital Marketing.jpg",
     
   },
     {
     title: "signage Design 2",
-    image: "/public/assets/signage design/Behance-cover food.jpg",
+    image: "/assets/signage design/Behance-cover food.jpg",
 
   },
     {
     title: "signage Design 3",
-    image: "/public/assets/signage design/Behance-cover-Cleaning.jpg",
+    image: "/assets/signage design/Behance-cover-Cleaning.jpg",
 
   },
     {
     title: "signage Design 4",
-    image: "/public/assets/signage design/portfolio-06.jpg",
+    image: "/assets/signage design/portfolio-06.jpg",
 
   },
     {
     title: "signage Design 5",
-    image: "/public/assets/signage design/Behance-cover.jpg",
+    image: "/assets/signage design/Behance-cover.jpg",
 
   },
     {
     title: "signage Design 6",
-    image: "/public/assets/signage design/Fn.resources_Poster 01.png",
+    image: "/assets/signage design/Fn.resources_Poster 01.png",
 
   },
     {
     title: "signage Design 7",
-    image: "/public/assets/signage design/portfolio-02.jpg",
+    image: "/assets/signage design/portfolio-02.jpg",
 
   },
     {
     title: "signage Design 8",
-    image: "/public/assets/signage design/portfolio-04.jpg",
+    image: "assets/signage design/portfolio-04.jpg",
 
   },
     {
     title: "signage Design 9",
-    image: "/public/assets/signage design/portfolio-03.jpg",
+    image: "/assets/signage design/portfolio-03.jpg",
 
   },
     {
     title: "signage Design 10",
-    image: "/public/assets/signage design/portfolio-05.jpg",
+    image: "/assets/signage design/portfolio-05.jpg",
 
   },
 
@@ -284,52 +284,52 @@ export const socialMediaDesignData = [
     images: [
   {
     title: "social media 1",
-    image: "/public/assets/social media/cosmetic ads.png",
+    image: "/assets/social media/cosmetic ads.png",
     
   },
     {
     title: "social media 2",
-    image: "/public/assets/social media/food ads 2.png",
+    image: "/assets/social media/food ads 2.png",
 
   },
     {
     title: "social media 3",
-    image: "/public/assets/social media/food ads.png",
+    image: "/assets/social media/food ads.png",
     
   },
     {
     title: "social media 4",
-    image: "/public/assets/social media/gadget ads 4.png",
+    image: "/assets/social media/gadget ads 4.png",
 
   },
     {
     title: "social media 5",
-    image: "/public/assets/social media/gadget ads 3.png",
+    image: "/assets/social media/gadget ads 3.png",
 
   },
     {
     title: "social media 6",
-    image: "/public/assets/social media/gadget ads.png",
+    image: "/assets/social media/gadget ads.png",
 
   },
     {
     title: "social media 7",
-    image: "/public/assets/social media/gadget ads 2.png",
+    image: "/assets/social media/gadget ads 2.png",
     
   },
     {
     title: "social media 8",
-    image: "/public/assets/social media/power tool ads.png",
+    image: "/assets/social media/power tool ads.png",
     
   },
     {
     title: "social media 9",
-    image: "/public/assets/social media/t shirt 3.png",
+    image: "/assets/social media/t shirt 3.png",
     
   },
     {
     title: "social media 10",
-    image: "/public/assets/social media/t shirt 4 behance.png",
+    image: "/assets/social media/t shirt 4 behance.png",
     
   },
 
